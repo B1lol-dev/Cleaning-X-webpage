35f56b36aa75f5e6cedb3c9cd276736e2db3e80c
B1lol-dev <sardorovichgamer@gmail.com>
2025-02-02 21:43:29 +0500

    help me

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // components
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "./components/ui/Header";
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
 
-  (() => {
+  useEffect(() => {
     setCartCount(0);
-  })();
+  }, []);
 
   return (
     <>
